95e0ff931ceb9dbf4abbace68adc3208231c6368
Michal Szyszka <m.szyszka@clickmaster.pl>
2019-05-28 13:01:43 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-// Bootstrap carousel
-$('.carousel').carousel({
-    interval: 3000
-})
+// Start carousel
+$(document).ready(function() {
+        //Initializes the carousel 
+        $('.carousel').carousel({
+            interval: 3000,
+            pause: "hover"
+        })
+        // //Stop it when window width is > 768px
+        // const windowWidth = $(window).width();
+        // if(windowWidth >= 768){
+        //     $('.carousel').carousel('pause'); 
+        //     console.log('over 768px');
+        // }
+    }
+);
+// //Add class to carousel item
+// $(document).ready(function() {
+//     let windowWidth = $(window).width();
+//     if(windowWidth >= 768) {
+//         $('.no-active').addClass('active');
+//     }
+// });
+
+// // Remove class from carousel item when width is < 768
+// $(window).on('resize', function(){
+//     const windowWidth = $(window).width();
+//     if(windowWidth < 768) {
+//         $('.no-active').removeClass('active');
+//     }
+// });
