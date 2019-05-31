12a7fa5bc7d7d9ae9e63dcf4499aa9746a922c03
Michal <mszyszka223@gmail.com>
2019-05-31 11:56:01 +0200

    update

--- script.js
+++ script.js
@@ -2,36 +2,15 @@ $(window).on('resize', setSectionHeight);
 $(window).on('load', checkSectionHeight);
 
 
-// Start carousel
-$(document).ready(function() {
-        //Initializes the carousel 
-        $('.carousel').carousel({
-            interval: 3000,
-            pause: "hover"
-        })
-        //Stop it when window width is > 768px
-        // const windowWidth = $(window).width();
-        // if(windowWidth >= 768){
-        //     $('.carousel').carousel('pause'); 
-        //     console.log('over 768px');
-        // }
-    }
-);
-// //Add class to carousel item
+// // Start carousel
 // $(document).ready(function() {
-//     let windowWidth = $(window).width();
-//     if(windowWidth >= 768) {
-//         $('.no-active').addClass('active');
+//         //Initializes the carousel 
+//         $('.carousel').carousel({
+//             interval: 3000,
+//             pause: "hover"
+//         })
 //     }
-// });
-
-// // Remove class from carousel item when width is < 768
-// $(window).on('resize', function(){
-//     const windowWidth = $(window).width();
-//     if(windowWidth < 768) {
-//         $('.no-active').removeClass('active');
-//     }
-// });
+// );
 
 //Check section.confirm-your-presence height and set it to content height
 function checkSectionHeight(){
